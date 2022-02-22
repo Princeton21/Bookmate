1661dbe47ff56e69887edc0ab690365d5679f037
Princeton <princeton2102@gmail.com>
2022-02-22 19:01:58 +0530

    Added files

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,20 @@ import Home from './Home';
 import Donate from './Donate';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import Menu from './Menu';
 
 function App() {
   return (
     <>
     <Navbar/>
+    {/* <Menu/> */}
     <Routes>
       <Route exact path ="/" element = {<Home/>} />
       <Route exact path ="/buy" element = {<Buy/>} />
       <Route exact path ="/contact" element = {<Contact/>} />
       <Route exact path ="/donate" element = {<Donate/>} />
     </Routes>
-    <Footer/>
+    {/* <Footer/> */}
    
     </>
   );
