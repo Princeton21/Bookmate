5f677b9239a79ba4a46de42b2c97b46246c10a26
Princeton <princeton2102@gmail.com>
2022-02-22 19:04:25 +0530

    Dropdown menu

---
+++ src/Menu.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './CSS/Menu.css';
+const Menu = () => {
+  return (
+    <>
+      <div className="menu-list">
+        <ul>
+          <li>Categories</li>
+          <li>Novels</li>
+          <li>Classics</li>
+          <li>Fiction</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Menu
