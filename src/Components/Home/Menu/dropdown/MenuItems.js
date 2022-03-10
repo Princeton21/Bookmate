8d1da3897dea20e429907533cf85dad41c944fce
Princeton <princeton2102@gmail.com>
2022-03-10 23:38:15 +0530

    data file added

---
+++ src/Components/Home/Menu/dropdown/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Action',
+        path:  '/action',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Drama',
+        path:  '/drama',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Sci-fi',
+        path:  '/action',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Crime,Suspense&Thriller',
+        path:  '/crmsusthrlr',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Classics',
+        path:  '/classics',
+        cName: 'dropdown-link'
+    },
+];
