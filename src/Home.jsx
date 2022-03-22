1a7974129af280b4e6830777646990b101ad5c20
Princeton <princeton2102@gmail.com>
2022-03-22 10:43:09 +0530

    .

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import "./CSS/Home.css";
-import Menu from './Components/Home/Menu/Menu';
 import MegaMenu from './Components/Home/Menu/MegaMenu';
-// import CarouselContainer from "./Components/Home/Menu/CarouselContainer"
 const Home = () => {
   return (
     <>
     {/* <Menu/> */}
     <MegaMenu/>
 
-    {/* <CarouselContainer/> */}
     <div className="home">
       </div>
     </>
