29696788432c33f845f43a767101286719eddbcd
Princeton <princeton2102@gmail.com>
2022-03-23 11:53:08 +0530

    changed tag to link

--- src/Components/Home/Menu/MegaMenu.jsx
+++ src/Components/Home/Menu/MegaMenu.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
 import './MegaMenu.css'
 import img from './assets/images/12.jpg'
 const MegaMenu = () => {
+  const [click, setClick] = useState(false);
+  const handleClick  = () => setClick(!click);
+
+
   return (
   <>
   <div className="menu-body"></div>
@@ -10,7 +14,7 @@ const MegaMenu = () => {
       <ul class="menu-links">
         <label for="close-btn" class="btn close-btn"><i class="fas fa-times"></i></label>
         <li>
-          <a href="#" class="desktop-item">Mega Menu</a>
+          <Link to="/" class="desktop-item" onClick={handleClick}>Categories</Link>
           <label for="showMega" class="mobile-item">Mega Menu</label>
           <div class="mega-box">
             <div class="content">
@@ -20,36 +24,36 @@ const MegaMenu = () => {
               <div class="row">
                 <h2>Genres</h2>
                 <ul class="mega-links">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Thriller</a></li>
-                  <li><a href="#">Romance</a></li>
-                  <li><a href="#">Sci fi</a></li>
+                  <li><Link to="/action">Action</Link></li>
+                  <li><Link to="/thriller">Thriller</Link></li>
+                  <li><Link to="/romance">Romance</Link></li>
+                  <li><Link to="/sci-fi">Sci fi</Link></li>
                 </ul>
               </div>
               <div class="row">
                 <h2>Year</h2>
                 <ul class="mega-links">
-                  <li><a href="#">FE</a></li>
-                  <li><a href="#">SE</a></li>
-                  <li><a href="#">TE</a></li>
-                  <li><a href="#">BE</a></li>
+                  <li><Link to="/">FE</Link></li>
+                  <li><Link to="/">SE</Link></li>
+                  <li><Link to="/">TE</Link></li>
+                  <li><Link to="/">BE</Link></li>
                 </ul>
               </div>
               <div class="row">
                 <h2>Subjects</h2>
                 <ul class="mega-links">
-                  <li><a href="#">Mathematics</a></li>
-                  <li><a href="#">Data Structures</a></li>
-                  <li><a href="#">Software Engg</a></li>
-                  <li><a href="#">Digital Electronics</a></li>
+                  <li><Link to="/mathematics">Mathematics</Link></li>
+                  <li><Link to="/datastructures">Data Structures</Link></li>
+                  <li><Link to="/software_eng">Software Engg</Link></li>
+                  <li><Link to="/digital_elec">Digital Electronics</Link></li>
                 </ul>
               </div>
             </div>
           </div>
         </li>
-        <li><Link to="/">Best Sellers</Link></li>
-        <li><Link to="/">Trending</Link></li>
-        <li><Link to="/">New Releases</Link></li>
+        <li><Link to="/bestsellers">Best Sellers</Link></li>
+        <li><Link to="/trending">Trending</Link></li>
+        <li><Link to="/newreleases">New Releases</Link></li>
       </ul>
       <label for="menu-btn" class="btn menu-btn"><i class="fas fa-bars"></i></label>
     </div>
