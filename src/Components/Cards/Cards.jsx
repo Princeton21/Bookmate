8e04a82ec5a1f13ce715be52e3f5728586d43882
Princeton <princeton2102@gmail.com>
2022-03-30 11:56:18 +0530

    Added separate data file for cards

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -2,18 +2,8 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card } from 'react-bootstrap';
 import './Cards.css'
-import img1 from '../Home/Menu/assets/images/12.jpg'
-import img2 from '../Home/Menu/assets/images/13.jpg'
-import img3 from '../Home/Menu/assets/images/14.jpg'
+import { cardInfo } from './Card_data';
 const Cards = () => {
-    const cardInfo = [
-        {image:img1,title:"Book number 1",text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, sapiente?" },
-        {image:img2,title:"Book number 2",text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, sapiente?"},
-        {image:img3,title:"Book number 3",text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, sapiente?"},
-        {image:img1,title:"Book number 4",text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, sapiente?"},
-    ];
-
-
     const renderCard = (card, index) => {
         return (
             <div className="card-single">
