e3fa0eecd88e219321456aaf2c7479b213bac8e3
Princeton <princeton2102@gmail.com>
2022-04-12 22:36:40 +0530

    added firebase utilities

---
+++ src/Firebase/Config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD4cpfb39sBz_t6KYFQScqrKWZ99qWk1Wk",
+    authDomain: "bookmate-0o.firebaseapp.com",
+    projectId: "bookmate-0o",
+    storageBucket: "bookmate-0o.appspot.com",
+    messagingSenderId: "1071385411930",
+    appId: "1:1071385411930:web:88b68199b119e851213363",
+    measurementId: "G-MX7HLW4L5Y"
+  };
