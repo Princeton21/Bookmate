637a9e7b00654b45e62d1f04b43b7270558ec3e1
Princeton <princeton2102@gmail.com>
2022-04-14 22:33:20 +0530

    login page added

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignIn from './Signin/SignIn'
+import './Login.css'
+const Login = () => {
+  return (
+    <>
+    <SignIn/>
+    </>
+  )
+}
+
+export default Login
