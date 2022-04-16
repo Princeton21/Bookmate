966c26165e21d9af4872d54114ce175f00696dd1
Kritika <kritikanc2003@gmail.com>
2022-04-16 22:12:09 +0530

    Made Login page

--- src/Components/Login/Forms/Button/Button.jsx
+++ src/Components/Login/Forms/Button/Button.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Button.css'
+import {FcGoogle} from 'react-icons/fc';
 const Button = ({children, ...otherProps}) => {
   return (
     <>
     <button className='btn' {...otherProps}>
+    <span className="navbar-toggler-icon" ><FcGoogle size={20} /></span>
+      Sign In with Google
     </button>
 
     </>
