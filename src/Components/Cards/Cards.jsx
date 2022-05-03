884fbd2884863015e8628e29a4f7f02e33ca6c02
Princeton <princeton2102@gmail.com>
2022-05-04 03:15:56 +0530

    added the retrieved data in cards

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,57 +1,49 @@
-// import React from 'react'
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Card } from 'react-bootstrap';
-// import './Cards.css'
-// import { cardInfo } from './Card_data';
-// const Cards = () => {
-//     const renderCard = (card, index) => {
-//         return (
-//             <div className="card-single">
-//           <Card style={{ width: "18rem" }} key={index} >
-//             <Card.Img variant="top"src={card.image} />
-//             <Card.Body>
-//               <Card.Title>{card.title}</Card.Title>
-//               <Card.Text>{card.text}</Card.Text>
-//             </Card.Body>
-//           </Card>
-//           </div>
-
-//         );
-//       };
-//     return <div className="card-main-body">{cardInfo.map(renderCard)}</div>;
-// }
-
-// export default Cards
 import React from 'react'
+import { useState,useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import './Cards.css'
+import {collection , getDocs} from "firebase/firestore"
+import { db } from '../../Firebase/Utilities'
+
+
 import { cardInfo } from './Card_data';
 const Cards = () => {
-    const renderCard = (card, index) => {
-        return (
-          <>
-            <div className='card-container'>
-              <div className="card-image">
-              <img src={card.image} alt="This is  book image"/>
+  const [users,setUsers] = useState([]);
+  const usersCollectionRef = collection(db,"books");
+
+  useEffect(() =>{
+      const getUsers = async () => {
+          const data = await getDocs(usersCollectionRef);
+          setUsers(data.docs.map((doc) => ({...doc.data(), id:doc.id})));
+      }
+      getUsers();
+  },[]);
+   
+    return <div className="card-main-body">{users.map((user) =>{
+      return ( 
+        <>
+          <div className='card-container'>
+            {/* <div className="card-image">
+            <img src={user.image} alt="This the book image"/>
+            </div> */}
+            <div className="card-body">
+              <div className="card-title">
+              <h3>{user.book}</h3>
               </div>
-              <div className="card-body">
-                <div className="card-title">
-                <h3>{card.title}</h3>
-                </div>
-                <div className="card-text">
-                    <p>{card.text}</p>
-                </div>
+              <div className="card-text">
+                  <p>{user.price}-Rs</p>
+                  <p>{user.description}</p>
               </div>
-              <div className="card-btn">
-                <button>
-                  <Link to='/' id='card-btn-link'>View more</Link>
-                </button>
-                </div>
             </div>
-          </>
-        );
-      };
-    return <div className="card-main-body">{cardInfo.map(renderCard)}</div>;
+            <div className="card-btn">
+              <button>
+                <Link to='/' id='card-btn-link'>View more</Link>
+              </button>
+              </div>
+          </div>
+        </>
+      );
+    })}</div>;
 }
 
 export default Cards
