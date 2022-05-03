13733d81320000022f9622fb4bba28fa2d127778
Sharayu <sharayu2903@gmail.com>
2022-05-03 22:46:32 +0530

    Main Cards added

---
+++ src/Components/Cards/CardsMain.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import "./CardsMain.css"
+const CardsMain = () => {
+  return (
+    <body>
+        <div className="container">
+            <main className="grid">
+
+                <article>
+                    <div className="text">
+                        <h3>FE</h3>
+                        <img src={require("../Home/assets/images/1.jpg")} alt='FE'/>
+                        <button >View More</button>
+                    </div>
+                </article>
+
+                <article>
+                    
+                    <div className="text">
+                        <h3>SE</h3>
+                        <img src={require("../Home/assets/images/12.jpg")} alt='SE'/>
+                        <button>View More</button>
+                    </div>
+                </article>
+
+                <article>
+                    
+                    <div className="text">
+                        <h3>TE</h3>
+                        <img src={require("../Home/assets/images/13.jpg")} alt='TE'/>
+                        <button>View More</button>
+                    </div>
+                </article>
+
+                <article>
+                    
+                    <div className="text">
+                        <h3>BE</h3>
+                        <img src={require("../Home/assets/images/14.jpg")} alt='BE'/>
+                        <button>View More</button>
+                    </div>
+                </article>
+
+                
+            </main>
+        </div>
+    </body>
+  )
+}
+
+export default CardsMain
--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,7 @@ import MegaMenu from './Components/Home/Menu/MegaMenu';
 import Cards from './Components/Cards/Cards';
 import Slider from './Components/Slider/Slider';
 import Footer from './Footer';
+import CardsMain from './Components/Cards/CardsMain';
 const Home = () => {
   return (
     <>
@@ -20,7 +21,7 @@ const Home = () => {
       <div className="home-cards">
         <Cards/>
       </div>
-
+    <CardsMain/>
     </div>
     <Footer/>
     </>
