dd070edccd6e90af4445e112bc732eca4c820cb9
Kritika <kritikanc2003@gmail.com>
2022-05-03 23:28:43 +0530

    added css to sell form

--- src/Components/Sell/Sellform.jsx
+++ src/Components/Sell/Sellform.jsx
@@ -7,20 +7,55 @@ function Sellform() {
         <div className="sell-box">
         <form>
             <h2>Post Advertisement</h2>
+            
+            <div className="form">
+            
+            <div className="class">
             <label>
-                Year:
+                Year* :
+                <br /> 
                 <input type="text" name="class" />
             </label>
+            </div>
+            <br />
+            
+
+            <div className="name">
             <label>
-                Name of the Book:
+                Name of the Book* : 
+                <br />
                 <input type="text" name="name" />
             </label>
+            </div>
+            <br />
+
+            <div className="name">
             <label>
-                Description:
-                
+                Price of the Book* : 
+                <br />
+                <input type="text" name="name" />
+            </label>
+            </div>
+            <br />
+            
+
+            <div className="describe">
+            <label>
+                Description* : 
+                <br />
                 <textarea placeholder='Describe about the book'></textarea>
             </label>
+            </div>
+            <br />
+            
+
+            <div className="button">
             <input type="submit" value="Post" />
+            </div>
+            <br />
+
+            </div>
+
         </form>
         </div>
         
