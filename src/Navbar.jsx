559ab07110709e9b7b189be07dd7653b956c2f2d
Kritika <kritikanc2003@gmail.com>
2022-05-09 12:49:38 +0530

    changed dropdown

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './CSS/Navbar.css';
 import { Link } from 'react-router-dom';
 import {FaBars} from 'react-icons/fa';
 
 const Navbar = () => {
+  
+  const [dropdown, setDropdown] = useState(false);
+
+  const handleClick = () => setDropdown(!dropdown);
+  
   return (
       <>
         <div className='header'>
@@ -27,6 +32,33 @@ const Navbar = () => {
             <li className="nav-item">
               <Link className="nav-link" to="/cart">cart</Link>
             </li>
+
+            <div classNameName="menu-body"></div>
+   <div className="main-menu">
+      <ul className="menu-links">
+        <li>
+          <Link to="/" className="desktop-item" onClick={handleClick}>Categories</Link>
+
+          <div className={dropdown ? "mega-bx-active":"mega-bx-inactive"}>
+          <div className="mega-box">
+            <div className="content">
+              <div className="row">
+                <h2>Department</h2>
+                <ul className="mega-links">
+                  <li><Link to="/ENTC">ENTC</Link></li>
+                  <li><Link to="/Computer">Computer</Link></li>
+                  <li><Link to="/Mechanical">Mechanical</Link></li>
+                  <li><Link to="/Civil">Civil</Link></li>
+                  <li><Link to="/Civil">AI</Link></li>
+                  <li><Link to="/Civil">IT</Link></li>
+                </ul>
+              </div>
+            </div> 
+            </div>
+          </div>
+        </li>
+        </ul>
+    </div>
             
           </ul>
         </div>
