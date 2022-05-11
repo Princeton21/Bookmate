c2ec2e9544b488cde41b6cef384da9e2f1166c70
Princeton <princeton2102@gmail.com>
2022-05-11 11:10:17 +0530

    Called component on sell page

--- src/Components/Sell/Sell.jsx
+++ src/Components/Sell/Sell.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../../Navbar'
 import Sellform from './Sellform'
+import Sellform1 from './Sellform1'
+import Sellformimg from './Sellformimg'
 
 const Sell = () => {
   return (
     <>
     <Navbar/>
+    {/* <Sellform1/> */}
     <Sellform/>
+    {/* <Sellformimg/> */}
     </>
     
   )
