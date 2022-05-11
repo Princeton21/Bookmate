e10ded88ae739b4d75d9cdab62b132d03b7aab36
Kritika <kritikanc2003@gmail.com>
2022-05-11 06:56:29 +0530

    login page changed

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import {Route , Routes} from  'react-router-dom';
 import Sell from './Components/Sell/Sell';
 import Cart from './Components/Carts Page/Cart'
 import Home from './Home';
-import Navbar from './Navbar';
-import Footer from './Footer';
+
+
 import Login from './Components/Login/Login';
+import Login1 from './login1/Login1';
+import Signup1 from './login1/Signup1';
+
 import { auth } from './Firebase/Utilities';
 import Contact from './Components/Contact us/Contact';
 import NotFound from './Components/404/NotFound';
+
 
 
 
@@ -45,9 +49,11 @@ class App extends Component {
       <>
       {/* <Navbar/> */}
       <Routes>
-        <Route exact path ="/" element = {<Home/>} />
+        <Route exact path ="/" element = {<Home user={this.state.user}/>} />
+
         <Route exact path ="/sell" element = {<Sell/>} />
-        <Route exact path ="/login" element = {<Login/>} />
+        <Route exact path ="/login1" element = {<Login1/>} />
+        <Route exact path ="/login1/Signup1" element = {<Signup1/>} />
         <Route exact path ="/contact" element = {<Contact/>} />
         <Route exact path = "/Cart" element = {
           <>
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import SignIn from './Signin/SignIn'
 import './Login.css'
 import Navbar from '../../Navbar'
-const Login = () => {
+const Login = ({user}) => {
   return (
     <>
-    <Navbar/>
+    <Navbar user={user}/>
     <SignIn/>
     </>
   )
