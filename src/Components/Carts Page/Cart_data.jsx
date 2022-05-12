ef1922b05c427e1244f621b1a7731f340b22e9bf
Princeton <princeton2102@gmail.com>
2022-05-12 17:46:27 +0530

    changed cards data

--- src/Components/Carts Page/Cart_data.jsx
+++ src/Components/Carts Page/Cart_data.jsx
@@ -3,8 +3,8 @@ import img2 from '../Home/assets/images/13.jpg'
 import img3 from '../Home/assets/images/14.jpg'
 
 export const cardInfo = [
-    {image:img1,title:"Book number 1",text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, sapiente?",price:"Price : 300 rs" },
-    {image:img2,title:"Book number 2",text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, sapiente?",price:"Price : 300 rs"},
-    {image:img3,title:"Book number 3",text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, sapiente?",price:"Price : 300 rs"},
-    {image:'https://media.gettyimages.com/photos/open-book-picture-id134301026',title:"Book number 4",text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, sapiente?",price:"Price : 300 rs"},
+    {image:img1,title:"Engineering Physics",text:"This book is very interesting",price:"Price : 300 rs" },
+    {image:img2,title:"Data Structures and Algorithms",text:"Topics explained deeply in this book",price:"Price : 300 rs"},
+    {image:img3,title:"Engineering Mathematics",text:"Problems with solutions available with this book",price:"Price : 300 rs"},
+    {image:'https://media.gettyimages.com/photos/open-book-picture-id134301026',title:"Software Engineering",text:"This book is filled with real life examples!",price:"Price : 300 rs"},
 ];
